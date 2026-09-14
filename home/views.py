b713dfa6d095a1f74ce97ed13245078eb14f021d
kavya-adusu <kavyaadusumi@gmail.com>
2026-09-13 22:14:29 -0400

    Fix home page title

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     template_data = {}
-    template_data['title'] = 'About'
+    template_data['title'] = 'Home'
     return render(request, 'home/index.html', 
                   {'template_data': template_data})
 def about(request):
